64b7900c9559c635d7b484967b4bcc8b4bb1ae77
Ingrid Ardine <ardineingrid@gmail.com>
2021-11-01 17:01:04 +0800

    Test: Modified app.py, connect to html

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
+f = open('index.html', 'r')
+app_layout = f.read()
+f.close()
+
 @app.route('/')
 def index():
-    return "Hello!" 
+    return app_layout  

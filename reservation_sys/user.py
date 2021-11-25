5dee40dedb5c8dd3576d69bc805e70d2ac47478f
Ingrid Ardine <ardineingrid@gmail.com>
2021-11-25 20:53:01 +0800

    rearrage folders, to be able to execute 'flask run' w/o moving up a directory, fixed reservation stuff

---
+++ reservation_sys/user.py
@@ -0,0 +1,14 @@
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+
+from reservation_sys.db import get_db
+
+from reservation_sys.auth import login_required
+
+bp = Blueprint('user', __name__, url_prefix='/user')
+
+@bp.route('/profile', methods=['GET'])
+@login_required
+def profile():
+    return render_template('user/profile.html')
